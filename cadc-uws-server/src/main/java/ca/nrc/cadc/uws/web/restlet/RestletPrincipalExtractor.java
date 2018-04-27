ff3939f2933f7a980387c169f6b8ac8d90afdbf3
Helena Jeeves <hridolfo@gmail.com>
2018-04-27 06:30:46 -0700

    s2247, response to code review

--- cadc-uws-server/src/main/java/ca/nrc/cadc/uws/web/restlet/RestletPrincipalExtractor.java
+++ cadc-uws-server/src/main/java/ca/nrc/cadc/uws/web/restlet/RestletPrincipalExtractor.java
@@ -1,40 +1,75 @@
 /*
- ************************************************************************
- ****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
- *
- * (c) 2012.                         (c) 2012.
- * National Research Council            Conseil national de recherches
- * Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
- * All rights reserved                  Tous droits reserves
- *
- * NRC disclaims any warranties         Le CNRC denie toute garantie
- * expressed, implied, or statu-        enoncee, implicite ou legale,
- * tory, of any kind with respect       de quelque nature que se soit,
- * to the software, including           concernant le logiciel, y com-
- * without limitation any war-          pris sans restriction toute
- * ranty of merchantability or          garantie de valeur marchande
- * fitness for a particular pur-        ou de pertinence pour un usage
- * pose.  NRC shall not be liable       particulier.  Le CNRC ne
- * in any event for any damages,        pourra en aucun cas etre tenu
- * whether direct or indirect,          responsable de tout dommage,
- * special or general, consequen-       direct ou indirect, particul-
- * tial or incidental, arising          ier ou general, accessoire ou
- * from the use of the software.        fortuit, resultant de l'utili-
- *                                      sation du logiciel.
- *
- *
- * @author jenkinsd
- * 4/20/12 - 12:44 PM
- *
- *
- *
- ****  C A N A D I A N   A S T R O N O M Y   D A T A   C E N T R E  *****
- ************************************************************************
- */
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2018.                            (c) 2018.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 5 $
+*
+************************************************************************
+*/
+
 package ca.nrc.cadc.uws.web.restlet;
 
 import ca.nrc.cadc.auth.AuthenticationUtil;
-import ca.nrc.cadc.auth.CookiePrincipal;
 import ca.nrc.cadc.auth.DelegationToken;
 import ca.nrc.cadc.auth.HttpPrincipal;
 import ca.nrc.cadc.auth.InvalidDelegationTokenException;
@@ -43,14 +78,12 @@ import ca.nrc.cadc.auth.SSOCookieCredential;
 import ca.nrc.cadc.auth.SSOCookieManager;
 import ca.nrc.cadc.auth.X509CertificateChain;
 import ca.nrc.cadc.net.NetUtil;
-import ca.nrc.cadc.util.ArrayUtil;
 import ca.nrc.cadc.util.StringUtil;
 
 import java.io.IOException;
 import java.security.AccessControlException;
 import java.security.Principal;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -104,16 +137,13 @@ public class RestletPrincipalExtractor implements PrincipalExtractor
                         "org.restlet.https.clientCertificates");
             if ((requestCertificates != null) && (!requestCertificates.isEmpty())) {
                 this.chain = new X509CertificateChain(requestCertificates);
-                if (this.chain != null) {
-                    principals.add(this.chain.getPrincipal());
-                }
+                principals.add(this.chain.getPrincipal());
             }
 
         }
         
         if (token == null)
         {
-            log.info("adding token");
             Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
             String tokenValue = headers.getFirstValue(AuthenticationUtil.AUTH_HEADER);
             if ( StringUtil.hasText(tokenValue) )
@@ -135,7 +165,7 @@ public class RestletPrincipalExtractor implements PrincipalExtractor
                 finally { }
             }
         }
-log.info("adding principals");
+
         // add HttpPrincipal
         final String httpUser = getAuthenticatedUsername();
         if (StringUtil.hasText(httpUser)) // user from HTTP AUTH
@@ -144,15 +174,15 @@ log.info("adding principals");
             principals.add(token.getUser());
         
         Series<Cookie> cookies = getRequest().getCookies();
-        log.info("cookie count: " + cookies.size());
-        log.info("principal count: " + principals.size());
-        log.info(principals);
+        log.debug("cookie count: " + cookies.size());
+        log.debug("principal count: " + principals.size());
+        log.debug(principals);
         if (cookies == null || (cookies.size() == 0))
             return;
         
         for (Cookie ssoCookie : cookies)
         {
-            log.info(ssoCookie.toString());
+            log.debug(ssoCookie.toString());
 
             if (SSOCookieManager.DEFAULT_SSO_COOKIE_NAME.equals(
                     ssoCookie.getName())
@@ -170,17 +200,12 @@ log.info("adding principals");
                     cookieCredentialList = ssoCookieManager.getSSOCookieCredentials(ssoCookie.getValue(),
                         NetUtil.getDomainName(getRequest().getResourceRef().toUrl()));
                 } 
-                catch (IOException e)
+                catch (InvalidDelegationTokenException | IOException e)
                 {
-                    log.info("Cannot use SSO Cookie. Reason: " 
+                    log.debug("Cannot use SSO Cookie. Reason: "
                             + e.getMessage());
-                } 
-                catch (InvalidDelegationTokenException e)
-                {
-                    log.info("Cannot use SSO Cookie. Reason: " 
-                            + e.getMessage());
+                    throw new AccessControlException("invalid SSO cookie" + e);
                 }
-                
             }
         }
     }
@@ -212,11 +237,6 @@ log.info("adding principals");
     {
         final String username;
 
-//        log.info("getrequest object: " + request);
-//        Request req = getRequest();
-//        log.info("req: "  + req);
-//        log.info("client info: " + req.getClientInfo());
-//        log.info("principals: " + req.getClientInfo().getPrincipals());
         if (!getRequest().getClientInfo().getPrincipals().isEmpty())
         {
             // Put in to support Safari not injecting a Challenge Response.
@@ -225,7 +245,7 @@ log.info("adding principals");
             // call to getRequest().getChallengeResponse().getIdentifier() would
             // return whatever username the caller provided in a non-authenticating call
             username = getRequest().getClientInfo().getPrincipals().get(0).getName();
-            log.info("username: " + username);
+            log.debug("username: " + username);
         }
         else 
         {
@@ -234,7 +254,6 @@ log.info("adding principals");
 
         return username;
     }
-
 
     public Request getRequest()
     {
