149479b929516e8cf128855c9ea04e8c44331087
Brian Major <major.brian@gmail.com>
2018-09-05 16:45:43 -0700

    extract runID from job for logging

--- cadc-uws-server/src/test/java/ca/nrc/cadc/uws/util/JobLogInfoTest.java
+++ cadc-uws-server/src/test/java/ca/nrc/cadc/uws/util/JobLogInfoTest.java
@@ -28,9 +28,9 @@
 
 package ca.nrc.cadc.uws.util;
 
-import junit.framework.Assert;
 
 import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
 
 import ca.nrc.cadc.log.WebServiceLogInfo;
@@ -46,17 +46,18 @@ public class JobLogInfoTest
         EasyMock.expect(job.getRemoteIP()).andReturn("192.168.0.0").once();
         EasyMock.expect(job.getRequestPath()).andReturn("/path/of/request").once();
         EasyMock.expect(job.getID()).andReturn("jobid").once();
+        EasyMock.expect(job.getRunID()).andReturn("runid").once();
         job.ownerSubject = null;
         
         EasyMock.replay(job);
         
         WebServiceLogInfo logInfo = new JobLogInfo(job);
         String start = logInfo.start();
-        Assert.assertEquals("Wrong start", "START: {\"method\":\"UWS\",\"path\":\"/path/of/request\",\"user\":\"anonUser\",\"from\":\"192.168.0.0\",\"jobID\":\"jobid\"}", start);
+        Assert.assertEquals("Wrong start", "START: {\"method\":\"UWS\",\"path\":\"/path/of/request\",\"user\":\"anonUser\",\"from\":\"192.168.0.0\",\"jobID\":\"jobid\",\"runID\":\"runid\"}", start);
         logInfo.setElapsedTime(1234L);
         logInfo.setMessage("the message");
         String end = logInfo.end();
-        Assert.assertEquals("Wrong end", "END: {\"method\":\"UWS\",\"path\":\"/path/of/request\",\"success\":true,\"user\":\"anonUser\",\"from\":\"192.168.0.0\",\"time\":1234,\"message\":\"the message\",\"jobID\":\"jobid\"}", end);
+        Assert.assertEquals("Wrong end", "END: {\"method\":\"UWS\",\"path\":\"/path/of/request\",\"success\":true,\"user\":\"anonUser\",\"from\":\"192.168.0.0\",\"time\":1234,\"message\":\"the message\",\"jobID\":\"jobid\",\"runID\":\"runid\"}", end);
         
         EasyMock.verify(job);
     }
