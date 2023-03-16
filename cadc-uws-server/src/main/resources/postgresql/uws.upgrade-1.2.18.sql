33b6d6f97df920bf01702dc299c74e85102fc374
Patrick Dowler <pdowler.cadc@gmail.com>
2023-03-16 16:15:02 -0700

    remove extraneous <tag> from upgrade-1.2.18

--- cadc-uws-server/src/main/resources/postgresql/uws.upgrade-1.2.18.sql
+++ cadc-uws-server/src/main/resources/postgresql/uws.upgrade-1.2.18.sql
@@ -1,5 +1,5 @@
 -- rename indices to make rollover maintenance cleaner
 
-ALTER INDEX <schema>.jobid_fkey RENAME TO jobdetail_fkey_<tag>;
-ALTER INDEX <schema>.<schema>_jobIndex_creationTime RENAME TO job_creationTime_<tag>;
-ALTER INDEX <schema>.<schema>_jobIndex_ownerID RENAME TO job_ownerID_<tag>;
+ALTER INDEX <schema>.jobid_fkey RENAME TO jobdetail_fkey;
+ALTER INDEX <schema>.<schema>_jobIndex_creationTime RENAME TO job_creationTime;
+ALTER INDEX <schema>.<schema>_jobIndex_ownerID RENAME TO job_ownerID;
