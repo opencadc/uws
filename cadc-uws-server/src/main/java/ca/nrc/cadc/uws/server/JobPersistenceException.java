958aa1d8a11bb01e484e17dc2377c7e4082fdd5c
Patrick Dowler <pdowler.cadc@gmail.com>
2023-10-12 10:37:23 -0700

    fix javadoc

--- cadc-uws-server/src/main/java/ca/nrc/cadc/uws/server/JobPersistenceException.java
+++ cadc-uws-server/src/main/java/ca/nrc/cadc/uws/server/JobPersistenceException.java
@@ -88,7 +88,7 @@ public class JobPersistenceException extends Exception
      * @param message the detail message (which is saved for later retrieval
      *                by the {@link #getMessage()} method).
      * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                {@link #getCause()} method).  (A null value is
      *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      * @since 1.4
